325d2619fb39f2e976b7544abf27fb98a917adff
админ <borodovgerman123@gmail.com>
2024-12-15 15:45:42 +0300

    Тесты для исключений

---
+++ TestProject1/ExceptionsTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MyCalc2;
+
+namespace CalculatorApp.Tests
+{
+    [TestClass]
+    public class ExceptionsTests
+    {
+        private Calculator calculator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InvalidExpression()
+        {
+            calculator.Calculate("2 + + 3");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void UnbalancedParentheses()
+        {
+            calculator.Calculate("(2 + 3 * 4");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivisionByZero()
+        {
+            calculator.Calculate("10 / 0");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Should_ThrowException_For_EmptyExpression()
+        {
+            calculator.Calculate("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ExtraNumbers()
+        {
+            calculator.Calculate("2 3 + 4");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void UnsupportedOperation()
+        {
+            calculator.Calculate("2 ^ 3");
+        }
+    }
+}
